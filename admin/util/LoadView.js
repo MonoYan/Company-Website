2dca6b513e50c896380dc0e79a6f689d0096d031
Mono Yan <yanshencc@gmail.com>
2023-07-18 22:59:48 +0800

    nav & sidemenu

--- admin/util/LoadView.js
+++ admin/util/LoadView.js
@@ -1,10 +1,38 @@
-if (localStorage.getItem("token")) {
-
-} else {
-    location.href = "/admin/views/login/index.html"
+function isLogin(){
+    return localStorage.getItem("token")
 }
 
-fetch("/admin/components/topbar/index.html").then(res => res.text()).then(res => {
-    console.log(res)
-    document.querySelector(".topbar").innerHTML = res
-})
+function renderTopbar(user){
+    let photo = document.querySelector('#topbar-photo')
+    let currentUsername = document.querySelector('#currentUsername')
+    let exit = document.querySelector('#exit')
+
+    photo.src = user.photo
+    currentUsername.innerHTML = user.username
+
+    exit.onclick = ()=>{
+        localStorage.removeItem("token")
+        location.href = "/admin/views/login/index.html"
+    }
+}
+async function load(id){
+    let user = isLogin()
+    if(user){
+        let topbarText = await fetch("/admin/components/topbar/index.html").then(res => res.text())
+        document.querySelector(".topbar").innerHTML = topbarText
+
+        renderTopbar(JSON.parse(user))
+        // console.log(user.photo)
+
+        let sidemenuText = await fetch("/admin/components/sidemenu/index.html").then(res => res.text())
+        document.querySelector(".sidemenu").innerHTML = sidemenuText
+
+        document.querySelector('#'+id).style.color = "#0a58ca"
+
+    }else {
+    location.href = "/admin/views/login/index.html"
+    }
+}
+
+
+export {load}
